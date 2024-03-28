e333aa4e4fa9327d33db482bb734da26a7685eaf
Omega Dziko F. <megaoy@yahoo.com>
2024-03-28 18:39:44 -0400

    last prb checked

--- aa02-pojo-basics/problems/02-values-in-object.js
+++ aa02-pojo-basics/problems/02-values-in-object.js
@@ -1,6 +1,6 @@
 /***********************************************************************
-Write a function called `valuesInObject(obj)` that takes in an object and returns 
-an array of all the values within that Object. 
+Write a function called `valuesInObject(obj)` that takes in an object and returns
+an array of all the values within that Object.
 
 
 Do this once using using a `for...in` loop and once using `Object.values`.
@@ -15,17 +15,17 @@ valuesInObject(foods); // => ["tart", "sour", "sweet"]
 ***********************************************************************/
 
 function valuesInObject(obj) {
-  // Your code here 
+  // Your code here
   let values1 = []
   // uses a for loop & interating over each key
-   for (let key in obj) {  
+   for (let key in obj) {
     values1.push(obj[key]);  //  We push the value associated with the current key into values1 Array
    }
    //using Object.values()
    // We retrieve an array containing the values of the object properties
    let values2 = Object.values(obj);
    // We are returniing both arrays
-   return [values1, values2];
+   return values1;
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
